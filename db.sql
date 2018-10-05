91c4aeeb98340acbe18536913a971da734777245
Edoardo <iweek16@gmail.com>
2018-10-05 14:11:08 +0200

    Added datetime to report

--- db.sql
+++ db.sql
@@ -55,13 +55,12 @@ CREATE TABLE IF NOT EXISTS report(
     user  varchar(100) NULL,
     type_report int not null,
     team int DEFAULT NULL,
+    date datetime not null,
     
     PRIMARY KEY(id),
     CONSTRAINT fk_report_type_report       FOREIGN KEY(type_report)         REFERENCES type_report(id),
-    CONSTRAINT fk_report_team              FOREIGN KEY(team)                REFERENCES team(id)
-    -- CONSTRAINT fk_report_city       FOREIGN KEY(city)       REFERENCES city(id),
+    CONSTRAINT fk_report_team              FOREIGN KEY(team)                REFERENCES team(id),
     CONSTRAINT fk_report_location   FOREIGN KEY(location)   REFERENCES location(id) ON DELETE CASCADE 
-    -- CONSTRAINT fk_report_user       FOREIGN KEY(user)   REFERENCES user(email)
 );
 
 CREATE TABLE IF NOT EXISTS history_report(
@@ -104,12 +103,6 @@ VALUES
 ('Smottamento manto stradale'),
 ('Igiene Urbana');
 
--- TRUNCATE location;
--- TRUNCATE report;
--- TRUNCATE city;
--- TRUNCATE user;
--- TRUNCATE photo;
-
 
 -- Coords --
 INSERT INTO location (lan, lng)
@@ -132,8 +125,8 @@ VALUES
 (40.536122, 17.433656),      -- Matteotti                       11
 (40.537869, 17.434836),      -- Ennio                           12
 (40.539320, 17.432186),      -- Diaz                            13  
-(40.536698, 17.438685)      -- Colombo                         14
-;
+(40.536698, 17.438685),      -- Colombo                         14
+(0.001, 0.001);
 
 
 
@@ -158,44 +151,44 @@ INSERT INTO team(name, type_report) VALUES
 
 
 -- Report --
-INSERT INTO report (city, description, address, location, user,grade,type_report,team)
+INSERT INTO report (city, description, address, location, user,grade,type_report,team,date)
 VALUES
-(1,'Descrizione prova 1','Via prova 1',11, null,'LOW',1,1),
-(1,'Descrizione prova 2','Via prova 2',11, null,'LOW',1,1),
-(1,'Descrizione prova 3','Via prova 3',11, null,'LOW',1,1),
-(1,'Descrizione prova 4','Via prova 4',11, null,'LOW',1,1),
-(1,'Descrizione prova 5','Via prova 5',11, null,'LOW',1,1),
+(1,'Descrizione prova 1','Via prova 1',15, null,'LOW',1,1,NOW()),
+(1,'Descrizione prova 2','Via prova 2',11, null,'LOW',1,1,NOW()),
+(1,'Descrizione prova 3','Via prova 3',11, null,'LOW',1,1,NOW()),
+(1,'Descrizione prova 4','Via prova 4',11, null,'LOW',1,1,NOW()),
+(1,'Descrizione prova 5','Via prova 5',11, null,'LOW',1,1,NOW()),NOW(),
 
-(1,'Descrizione prova 6','Via prova 6',11, null,'LOW',1,2),
-(1,'Descrizione prova 7','Via prova 7',11, null,'LOW',1,2),
+(1,'Descrizione prova 6','Via prova 6',11, null,'LOW',1,2,NOW()),
+(1,'Descrizione prova 7','Via prova 7',11, null,'LOW',1,2,NOW()),NOW(),
 
-(1,'Descrizione prova 8','Via prova 8',11, null,'LOW',1,3),
-(1,'Descrizione prova 9','Via prova 9',11, null,'LOW',1,3),
-(1,'Descrizione prova 10','Via prova 10',11, null,'LOW',1,3),
-(1,'Descrizione prova 11','Via prova 11',11, null,'LOW',1,3),
-(1,'Descrizione prova 12','Via prova 12',11, null,'LOW',1,3),
-(1,'Descrizione prova 13','Via prova 13',11, null,'LOW',1,3),
-(1,'Descrizione prova 14','Via prova 14',11, null,'LOW',1,3),
-(1,'Descrizione prova 15','Via prova 15',11, null,'LOW',1,3),
-(1,'Descrizione prova 16','Via prova 16',11, null,'LOW',1,3),
+(1,'Descrizione prova 8','Via prova 8',11, null,'LOW',1,3,NOW()),
+(1,'Descrizione prova 9','Via prova 9',11, null,'LOW',1,3,NOW()),
+(1,'Descrizione prova 10','Via prova 10',11, null,'INTERMEDIATE',1,3,NOW()),
+(1,'Descrizione prova 11','Via prova 11',11, null,'INTERMEDIATE',1,3,NOW()),
+(1,'Descrizione prova 12','Via prova 12',11, null,'INTERMEDIATE',1,3,NOW()),
+(1,'Descrizione prova 13','Via prova 13',11, null,'INTERMEDIATE',1,3,NOW()),
+(1,'Descrizione prova 14','Via prova 14',11, null,'INTERMEDIATE',1,3,NOW()),
+(1,'Descrizione prova 15','Via prova 15',11, null,'INTERMEDIATE',1,3,NOW()),
+(1,'Descrizione prova 16','Via prova 16',11, null,'INTERMEDIATE',1,3,NOW()),NOW(),
 
-(1,'Descrizione prova 17','Via prova 17',11, null,'LOW',1,4),
-(1,'Descrizione prova 18','Via prova 18',11, null,'LOW',1,4),
-(1,'Descrizione prova 19','Via prova 19',11, null,'LOW',1,4),
-(1,'Descrizione prova 20','Via prova 20',11, null,'LOW',1,4),
-(1,'Descrizione prova 21','Via prova 21',11, null,'LOW',1,4),
-(1,'Descrizione prova 22','Via prova 22',11, null,'LOW',1,4),
+(1,'Descrizione prova 17','Via prova 17',11, null,'INTERMEDIATE',1,4,NOW()),
+(1,'Descrizione prova 18','Via prova 18',11, null,'INTERMEDIATE',1,4,NOW()),
+(1,'Descrizione prova 19','Via prova 19',11, null,'INTERMEDIATE',1,4,NOW()),
+(1,'Descrizione prova 20','Via prova 20',11, null,'HIGH',1,4,NOW()),
+(1,'Descrizione prova 21','Via prova 21',11, null,'HIGH',1,4,NOW()),
+(1,'Descrizione prova 22','Via prova 22',11, null,'HIGH',1,4,NOW()),NOW(),
 
-(1,'Descrizione prova 23','Via prova 23',11, null,'LOW',1,5),
-(1,'Descrizione prova 24','Via prova 24',11, null,'LOW',1,5),
-(1,'Descrizione prova 25','Via prova 25',11, null,'LOW',1,5),
-(1,'Descrizione prova 26','Via prova 26',11, null,'LOW',1,5),
-(1,'Descrizione prova 27','Via prova 27',11, null,'LOW',1,5),
-(1,'Descrizione prova 28','Via prova 28',11, null,'LOW',1,5),
-(1,'Descrizione prova 29','Via prova 29',11, null,'LOW',1,5),
-(1,'Descrizione prova 30','Via prova 30',11, null,'LOW',1,5),
-(1,'Descrizione prova 31','Via prova 31',11, null,'LOW',1,5),
-(1,'Descrizione prova 32','Via prova 32',11, null,'LOW',1,5);
+(1,'Descrizione prova 23','Via prova 23',11, null,'HIGH',1,5,NOW()),
+(1,'Descrizione prova 24','Via prova 24',11, null,'HIGH',1,5,NOW()),
+(1,'Descrizione prova 25','Via prova 25',11, null,'HIGH',1,5,NOW()),
+(1,'Descrizione prova 26','Via prova 26',11, null,'HIGH',1,5,NOW()),
+(1,'Descrizione prova 27','Via prova 27',11, null,'HIGH',1,5,NOW()),
+(1,'Descrizione prova 28','Via prova 28',11, null,'HIGH',1,5,NOW()),
+(1,'Descrizione prova 29','Via prova 29',11, null,'HIGH',1,5,NOW()),
+(1,'Descrizione prova 30','Via prova 30',11, null,'HIGH',1,5,NOW()),
+(1,'Descrizione prova 31','Via prova 31',11, null,'HIGH',1,5,NOW()),
+(1,'Descrizione prova 32','Via prova 32',11, null,'HIGH',1,5,NOW());
 
 
 
