3d298d3ff339ae64b4402cbf8e86f2b63411735e
Edoardo <iweek16@gmail.com>
2018-10-11 11:04:18 +0200

    Writing Admin class

--- php/classes/admin.php
+++ php/classes/admin.php
@@ -4,6 +4,21 @@
 
 class Admin{
     public $name;
+    private $reports = array();
+
+
+    function __constructor($name){
+        $this->name = name;
+    }
+
+
+    function deleteReport($id){
+        
+        $tmp = $reports[$id];
+        unset($repors[$id]);
+        return $tmp->deleteReport();
+    }
+
 
     
 }
