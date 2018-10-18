65124cf5b89596c27f84dccd804945803dfc1f65
Edoardo Cavallo <e.cavallo13@studenti.uniba.it>
2018-10-18 14:59:39 +0200

    Starting implementing ente

--- js/ente.js
+++ js/ente.js
@@ -1,15 +1,28 @@
+// ============== GET
+const URL_FETCH_REPORTS_BY_ENTE =  'apiReport/ente/'
+
 class Ente extends Admin{
     constructor(name){
         super(name)
-    }
 
-
-    fetchInfo(){
-
+        this.teams = []
+        this.fetchTeams()
     }
 
     fetchTeams(){
+        Hub.connect(URL_FETCH_REPORTS_BY_ENTE, 'GET', null, {
+            onsuccess: (result)=>{
+                result = JSON.parse(result.response)
+                // this.typeReports = result.map(t=>t.name)
+                for(let type of result.data){
 
+                    for(let team of type.teams){
+                        this.teams.push( new Team(team.name, team.nMember, team.reports) )
+                    }
+
+                }
+            }
+        })
     }
 
     editTeam(){} // nome e numero di componenti
