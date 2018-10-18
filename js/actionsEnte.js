3acc98e84e061cefe5086c0338ea76234d62b864
Edoardo Cavallo <e.cavallo13@studenti.uniba.it>
2018-10-18 08:56:32 +0200

    Split file of actions option for ente and team

---
+++ js/actionsEnte.js
@@ -0,0 +1,3 @@
+// ====== Actions for ente
+const ACTION_OPTION_DELETE_REPORT               = new ActionOption('Elimina segnalazione',      ()=>{console.log('Elimina segnalazione')}, "Cancella la segnalazione permanentemente",true)
+const ACTION_OPTION_CHANGE_TEAM                 = new ActionOption('Cambia team',               ()=>{console.log('Cambio team')}, "Assegna un'altro team a questa segnalazione")
--- js/actionsTeam.js
+++ js/actionsTeam.js
@@ -1,14 +1,10 @@
-// ====== Actions for ente
-const ACTION_OPTION_DELETE_REPORT               = new ActionOption('Elimina segnalazione',      team.deleteReport.bind(team), "Cancella la segnalazione permanentemente",true)
-const ACTION_OPTION_CHANGE_TEAM                 = new ActionOption('Cambia team',               ()=>{console.log('Cambio team')}, "Assegna un'altro team a questa segnalazione")
-
-
 // ====== Actions for team
-// const ACTION_OPTION_DELETE_REPORT  => already defined ↑ ↑
+
 const ACTION_OPTION_SET_REPORT_AS_INCHARGE      = new ActionOption('Prendi in carica',        team.setReportAsInCharge.bind(team),      "Imposta la segnalazione come in lavorazione")
 const ACTION_OPTION_SET_REPORT_AS_DONE          = new ActionOption('Chiudi segnalazione',     team.setReportAsDone.bind(team),          "Imposta la segnalazione come finita")
 const ACTION_OPTION_UPDATE_HISTORY_REPORT       = new ActionOption('Aggiorna progresso nota', team.updateHistoryOfReport.bind(team),    "Inserisci una nota allo storico della segnalazione")
 const ACTION_OPTION_RE_SET_REPORT_AS_INCHARGE   = new ActionOption('Riporta in lavorazione',  team.reSetReportAsInCharge.bind(team),    "Reimposta la segnalzione come in lavorazione")
+const ACTION_OPTION_DELETE_REPORT               = new ActionOption('Elimina segnalazione',      team.deleteReport.bind(team), "Cancella la segnalazione permanentemente",true)
 const ACTION_OPTION_DELETE_REPORTS              = new ActionOption('Elimina le segnalazione selezionate',      team.deleteReports.bind(team), "Cancella le segnalazione permanentemente",true)
 
 const ACTIONS_TEAM_REPORT_AS_WAITING = [
